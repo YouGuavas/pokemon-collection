c54141523f6f09bbd07a6e2d03538f9995b85153
Patrick Yambrick <patrickyambrick@gmail.com>
2023-02-22 17:27:22 -0500

    Add snipcart shopping cart

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,23 @@
 import Link from 'next/link';
 import {FaShoppingCart} from 'react-icons/fa';
+import { useState, useEffect } from 'react';
 
 export default function Nav() {
+    const [total, setTotal] = useState(0);
+    useEffect(() => {
+        if (window.Snipcart) {
+            setTotal(Snipcart.store.getState().cart.total);
+        }
+    })
     return (
         <nav>
             <Link href="/">Home</Link>
-            <Link href="/Cart"><FaShoppingCart /></Link>
+            <Link href="#" className="snipcart-checkout snipcart-summary"><FaShoppingCart /><span className="snipcart-total-price">
+              {new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+              }).format(total)}
+            </span> </Link>
         </nav>
     )
 }
